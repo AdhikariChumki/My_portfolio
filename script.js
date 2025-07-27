6aa6a321c5d0e2aa3dd48d391fe18131a178ddc9
AdhikariChumki <chumkia17@gmail.com>
2025-07-27 12:48:57 +0530

    update mobile hamburger menu

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Typing animation for headline using Typed.js
-document.addEventListener("DOMContentLoaded", function () {
-  const typed = new Typed("#typed", {
-    strings: [
-      "Crafting seamless UI test automation with Selenium & Cucumber",
-      "Validating robust APIs using RestAssured & Postman",
-      "Optimizing pipelines with Jenkins, Docker & AWS"
-    ],
-    typeSpeed: 50,
-    backSpeed: 30,
-    loop: true
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Typing animation
+    const typed = new Typed("#typed", {
+      strings: [
+        "Crafting seamless UI test automation with Selenium & Cucumber",
+        "Validating robust APIs using RestAssured & Postman",
+        "Optimizing pipelines with Jenkins, Docker & AWS"
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true
+    });
+
+    // Toggle mobile menu
+    const toggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    toggle.addEventListener("click", function () {
+      navLinks.classList.toggle("active");
+    });
   });
-});
+
